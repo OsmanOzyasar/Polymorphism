c9b1fa9693930452610bf3068b36a7da42ee9dcc
osmanozysr <123673331+OsmanOzyasar@users.noreply.github.com>
2025-01-09 15:02:51 +0300

    Add project files.

---
+++ BaseGeometrikSekil.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public abstract class BaseGeometrikSekil
+    {
+        public int Height {  get; set; }
+        public int Width { get; set; }
+
+        public virtual double AlanHesapla(int height, int width)
+        {
+            Height = height; 
+            Width = width;
+            return (double) height * width;
+        }
+    }
+
+    public class Triangle : BaseGeometrikSekil
+    {
+        public override double AlanHesapla(int height, int width)
+        {
+            return (double)(height * width) / 2;
+        }
+    }
+
+    public class Square : BaseGeometrikSekil
+    {
+        public override double AlanHesapla(int height, int width)
+        {
+            return base.AlanHesapla(height, width);
+        }
+    }
+
+    public class Ractangle : BaseGeometrikSekil
+    {
+        public override double AlanHesapla(int height, int width)
+        {
+            return base.AlanHesapla(height, width);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Drawing;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Square square = new Square();
+            Console.WriteLine("Kare Alanı: " + square.AlanHesapla(5, 5) + " cm kare"); 
+
+            BaseGeometrikSekil sekil = new Triangle();
+            Console.WriteLine("Üçgen alanı: " + sekil.AlanHesapla(7, 8) + " cm kare");
+
+            Ractangle ractangle = new Ractangle();
+            Console.WriteLine("Dikdörtgen alanı: " + ractangle.AlanHesapla(12,8) + " cm kare");
+        }
+    }
+}
